19ed5c9f46f4ce72590940868c763807d07dd33f
JojoDom <adwoa_dadson@yahoo.com>
2024-01-04 10:47:42 +0000

    added firebase background and foreground message listener and handler

--- taskmaker/lib/main.dart
+++ taskmaker/lib/main.dart
@@ -1,17 +1,69 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+import 'package:logger/logger.dart';
+import 'package:taskmaker/widgets/show_notification.dart';
+import 'firebase_options.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+   FirebaseMessaging messaging = FirebaseMessaging.instance;
+  FlutterLocalNotificationsPlugin? flutterLocalNotificationsPlugin;
+  NotificationSettings settings = await messaging.requestPermission(
+  alert: true,
+  announcement: false,
+  badge: true,
+  carPlay: false,
+  criticalAlert: false,
+  provisional: false,
+  sound: true,
+);
+print('User granted permission: ${settings.authorizationStatus}');
+  await FirebaseMessaging.instance.getInitialMessage();
+  FirebaseMessaging.instance.onTokenRefresh.listen((messaging) {
+    Logger().i('REFRESH ' + messaging);
+  }).onError((err) {});
+  messaging.getToken().then(
+    (value) {
+      Logger().i('TOKEN ' + value!);
+    },
+  );
+
+  // Lisitnening to the background messages
+Future<void> _firebaseMessagingBackgroundHandler(RemoteMessage message) async {
+  await Firebase.initializeApp();
+  print("Handling a background message: ${message.messageId}");
+}
+
+FirebaseMessaging.onBackgroundMessage(_firebaseMessagingBackgroundHandler);
+// Listneing to the foreground messages
+  
+FirebaseMessaging.onMessage.listen((RemoteMessage message) {
+    RemoteNotification? notification = message.notification;
+    print('Got a message whilst in the foreground!');
+    print('Message data: ${message.data}');
+      showNotification(
+          title: notification?.title,
+          body: notification?.body,
+          image: message.data["image"],
+          route: message.data["screen"]);    
+    if (message.notification != null) {
+      print('Message also contained a notification: ${message.notification}');
+    }
+  });
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
+    return GetMaterialApp(
+      title: 'DoMeNow',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
--- taskmaker/macos/Flutter/GeneratedPluginRegistrant.swift
+++ taskmaker/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -11,8 +11,10 @@ import firebase_analytics
 import firebase_core
 import firebase_crashlytics
 import firebase_messaging
+import flutter_local_notifications
 import path_provider_foundation
 import shared_preferences_foundation
+import sqflite
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   AssetsAudioPlayerPlugin.register(with: registry.registrar(forPlugin: "AssetsAudioPlayerPlugin"))
@@ -21,6 +23,8 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
   FLTFirebaseCrashlyticsPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCrashlyticsPlugin"))
   FLTFirebaseMessagingPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseMessagingPlugin"))
+  FlutterLocalNotificationsPlugin.register(with: registry.registrar(forPlugin: "FlutterLocalNotificationsPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
+  SqflitePlugin.register(with: registry.registrar(forPlugin: "SqflitePlugin"))
 }
