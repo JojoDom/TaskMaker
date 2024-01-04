a28ffdbc81f3207f69e0226e05e0498ce41b8fcb
JojoDom <adwoa_dadson@yahoo.com>
2024-01-04 09:50:39 +0000

    changed app name and added app screens

--- taskmaker/lib/main.dart
+++ taskmaker/lib/main.dart
@@ -30,8 +30,6 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold();
   }
 }
-
-
